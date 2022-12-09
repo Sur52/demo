381ff039512d7b212d8635fb3be3ab38ea501822
Shreyas. R <Shreyas. R@DELL-i7>
2022-12-09 18:15:03 +0530

    GitDemo

---
+++ src/test/java/demo/Test3.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	@Test
+	public void demo() {
+		Reporter.log("hello pythonSelenium", true);	
+	}
+
+}
